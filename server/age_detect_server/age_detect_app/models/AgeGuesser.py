e30a5a1d1e18566fd9f5d0fecfea026dba2b774d
oshirogo <dscripps@gmail.com>
2012-09-10 15:44:44 +0900

    test

--- server/age_detect_server/age_detect_app/models/AgeGuesser.py
+++ server/age_detect_server/age_detect_app/models/AgeGuesser.py
@@ -36,34 +36,34 @@ class AgeGuesser(models.Model):
         is_male_model.load("{0}train/data/is_male.data".format(settings.PROJECT_ROOT))
         
         #first, predict if male or female
-        is_male = (is_male_model.predict(image)[0] == 1)
-        #is_male = True
+        #is_male = (is_male_model.predict(image)[0] == 1)
+        is_male = True
         #confidence = predicted_label[1]
-#        if is_male:
-#            #load male age datasets
-#            is_youth_model.load("{0}train/data/male_is_youth.data".format(settings.PROJECT_ROOT))
-#            is_old_model.load("{0}train/data/male_is_old.data".format(settings.PROJECT_ROOT))
-#            decade_model.load("{0}train/data/male_decade.data".format(settings.PROJECT_ROOT))
-#        else:
-#            #load female age datasets
-#            is_youth_model.load("{0}train/data/female_is_youth.data".format(settings.PROJECT_ROOT))
-#            is_old_model.load("{0}train/data/female_is_old.data".format(settings.PROJECT_ROOT))
-#            decade_model.load("{0}train/data/female_decade.data".format(settings.PROJECT_ROOT))
-#        
-#        #guess age
-#        is_youth = (is_youth_model.predict(image)[0] == 1)
-#        is_old = (is_old_model.predict(image)[0]  == 1)
-#        
-#        
-#        
-#        if is_youth and not is_old:
-#            guessed_age = [0, 14, 14]
-#        elif is_old and not is_youth:
-#            guessed_age = [40, 60, random.randint(40, 50)]
-#        else:
-#            #image is neither young nor old, guess the decade
-#            decade = decade_model.predict(image)[0]
-#            guessed_age = [decade, decade+9, random.randint(decade, decade+9)]
+        if is_male:
+            #load male age datasets
+            is_youth_model.load("{0}train/data/male_is_youth.data".format(settings.PROJECT_ROOT))
+            is_old_model.load("{0}train/data/male_is_old.data".format(settings.PROJECT_ROOT))
+            decade_model.load("{0}train/data/male_decade.data".format(settings.PROJECT_ROOT))
+        else:
+            #load female age datasets
+            is_youth_model.load("{0}train/data/female_is_youth.data".format(settings.PROJECT_ROOT))
+            is_old_model.load("{0}train/data/female_is_old.data".format(settings.PROJECT_ROOT))
+            decade_model.load("{0}train/data/female_decade.data".format(settings.PROJECT_ROOT))
+        
+        #guess age
+        is_youth = (is_youth_model.predict(image)[0] == 1)
+        is_old = (is_old_model.predict(image)[0]  == 1)
+        
+        
+        
+        if is_youth and not is_old:
+            guessed_age = [0, 14, 14]
+        elif is_old and not is_youth:
+            guessed_age = [40, 60, random.randint(40, 50)]
+        else:
+            #image is neither young nor old, guess the decade
+            decade = decade_model.predict(image)[0]
+            guessed_age = [decade, decade+9, random.randint(decade, decade+9)]
             
         return guessed_age
     
