34ca473b1ac14d817ac60f71ae4689c0850c4471
oshirogo <dscripps@gmail.com>
2012-09-10 17:53:36 +0900

    fixing cv loading problem on server

--- server/age_detect_server/age_detect_app/models/AgeGuesser.py
+++ server/age_detect_server/age_detect_app/models/AgeGuesser.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import os
 import sys
+import cv2.cv as cv
 import cv2
 import numpy as np
 import glob
@@ -18,7 +19,8 @@ class AgeGuesser(models.Model):
 #        self.decade_model = cv2.createFisherFaceRecognizer()#14-19=10,20-29=20,30=39=30s,40-49=40
     
     def get_image(self, file):
-        im = cv2.imread(file, cv2.IMREAD_GRAYSCALE)
+        #im = cv2.imread(file, cv2.IMREAD_GRAYSCALE)
+        im = cv.LoadImageM(file, cv2.IMREAD_GRAYSCALE)
         image_data = np.asarray(im, dtype=np.uint8)
         return image_data
     
@@ -26,13 +28,15 @@ class AgeGuesser(models.Model):
         
         guessed_age = [0, 0, 0] #guessed age, min age, max age
         
+        
         is_male_model = cv2.createFisherFaceRecognizer()
         is_youth_model = cv2.createFisherFaceRecognizer()#is youth? (4-13 yes, 19+ no)
         is_old_model = cv2.createFisherFaceRecognizer()#is old (10-25 no, 40+ yes)
         decade_model = cv2.createFisherFaceRecognizer()#14-19=10,20-29=20,30=39=30s,40-49=40
         
         
-        #is_male_model.load("{0}train/data/is_male.data".format(settings.PROJECT_ROOT))
+        is_male_model.load("{0}train/data/is_male.data".format(settings.PROJECT_ROOT))
+        
         #first, predict if male or female
         #is_male = (is_male_model.predict(image)[0] == 1)
         is_male = True
@@ -40,28 +44,28 @@ class AgeGuesser(models.Model):
         if is_male:
             #load male age datasets
             is_youth_model.load("{0}train/data/male_is_youth.data".format(settings.PROJECT_ROOT))
-#            is_old_model.load("{0}train/data/male_is_old.data".format(settings.PROJECT_ROOT))
-#            decade_model.load("{0}train/data/male_decade.data".format(settings.PROJECT_ROOT))
-#        else:
-#            #load female age datasets
-#            is_youth_model.load("{0}train/data/female_is_youth.data".format(settings.PROJECT_ROOT))
-#            is_old_model.load("{0}train/data/female_is_old.data".format(settings.PROJECT_ROOT))
-#            decade_model.load("{0}train/data/female_decade.data".format(settings.PROJECT_ROOT))
-#        
-#        #guess age
-#        is_youth = (is_youth_model.predict(image)[0] == 1)
-#        is_old = (is_old_model.predict(image)[0]  == 1)
-#        
-#        
-#        
-#        if is_youth and not is_old:
-#            guessed_age = [0, 14, 14]
-#        elif is_old and not is_youth:
-#            guessed_age = [40, 60, random.randint(40, 50)]
-#        else:
-#            #image is neither young nor old, guess the decade
-#            decade = decade_model.predict(image)[0]
-#            guessed_age = [decade, decade+9, random.randint(decade, decade+9)]
+            is_old_model.load("{0}train/data/male_is_old.data".format(settings.PROJECT_ROOT))
+            decade_model.load("{0}train/data/male_decade.data".format(settings.PROJECT_ROOT))
+        else:
+            #load female age datasets
+            is_youth_model.load("{0}train/data/female_is_youth.data".format(settings.PROJECT_ROOT))
+            is_old_model.load("{0}train/data/female_is_old.data".format(settings.PROJECT_ROOT))
+            decade_model.load("{0}train/data/female_decade.data".format(settings.PROJECT_ROOT))
+        
+        #guess age
+        is_youth = (is_youth_model.predict(image)[0] == 1)
+        is_old = (is_old_model.predict(image)[0]  == 1)
+        
+        
+        
+        if is_youth and not is_old:
+            guessed_age = [0, 14, 14]
+        elif is_old and not is_youth:
+            guessed_age = [40, 60, random.randint(40, 50)]
+        else:
+            #image is neither young nor old, guess the decade
+            decade = decade_model.predict(image)[0]
+            guessed_age = [decade, decade+9, random.randint(decade, decade+9)]
             
         return guessed_age
     
